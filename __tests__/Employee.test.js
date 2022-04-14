8c8250ee00b121b133f73dc549841db1f80b8bcc
jrj-sys <jamesjamel99@gmail.com>
2022-04-14 17:24:47 -0500

    Employee class and methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,4 +7,28 @@ it('should create an Employee object with name, id, and email properties', () =>
     expect(employee.id).toEqual('12345');
     expect(employee.email).toEqual('email@email.com');
 });
-    
+
+it('should return the name of the Employee', () => {
+    const employee = new Employee('JJ', '12345', 'email@email.com');
+
+    expect(employee.getName()).toEqual('JJ');
+});
+
+it('should return the ID of the Employee', () => {
+    const employee = new Employee('JJ', '12345', 'email@email.com');
+
+    expect(employee.getId()).toEqual('12345');
+});
+
+it('should return the email of the Employee', () => {
+    const employee = new Employee('JJ', '12345', 'email@email.com');
+
+    expect(employee.getEmail()).toEqual('email@email.com');
+});
+
+it('should return `Employee`', () => {
+    const employee = new Employee('JJ', '12345', 'email@email.com');
+
+    expect(employee.getRole()).toEqual('Employee');
+});
+
